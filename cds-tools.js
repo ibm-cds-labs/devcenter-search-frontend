a770c4991549de0a95cf773dcfbbdb3c78057b8f
Glynn Bird <glynn.bird@gmail.com>
2015-04-21 16:52:46 +0100

    added search url

---
+++ cds-tools.js
@@ -0,0 +1,24 @@
+
+// generate a query string from a a search string and 
+// an array of filters
+var generateQueryString = function(searchText, filter) {
+  var qs = ""
+  
+  if(searchText && searchText.length>0) {
+    qs += "searchText="+encodeURIComponent(searchText);
+  }
+  if(filter && filter.length>0) {
+    qs += "&filter="+JSON.stringify(filter);
+  }
+  return qs;
+}
+
+
+
+// when the index page's search form is submitted
+var indexSearchSubmit = function() {
+  var searchText = $('#bigsearchinput').val();
+  var qs = generateQueryString(searchText,[]);
+  window.location.href="resources.html#?"+qs;
+  return false;
+}
--- cds.js
+++ cds.js
@@ -10,6 +10,84 @@ var sanitise = function(str) {
   return '"' + s + '"';
 }
 
+// from https://github.com/balupton/jquery-sparkle/blob/master/scripts/resources/core.string.js
+String.prototype.queryStringToJSON = String.prototype.queryStringToJSON || function ( )
+{	// Turns a params string or url into an array of params
+	// Prepare
+	var params = String(this);
+	// Remove url if need be
+	params = params.substring(params.indexOf('?')+1);
+	// params = params.substring(params.indexOf('#')+1);
+	// Change + to %20, the %20 is fixed up later with the decode
+	params = params.replace(/\+/g, '%20');
+	// Do we have JSON string
+	if ( params.substring(0,1) === '{' && params.substring(params.length-1) === '}' )
+	{	// We have a JSON string
+		return eval(decodeURIComponent(params));
+	}
+	// We have a params string
+	params = params.split(/\&(amp\;)?/);
+	var json = {};
+	// We have params
+	for ( var i = 0, n = params.length; i < n; ++i )
+	{
+		// Adjust
+		var param = params[i] || null;
+		if ( param === null ) { continue; }
+		param = param.split('=');
+		if ( param === null ) { continue; }
+		// ^ We now have "var=blah" into ["var","blah"]
+
+		// Get
+		var key = param[0] || null;
+		if ( key === null ) { continue; }
+		if ( typeof param[1] === 'undefined' ) { continue; }
+		var value = param[1];
+		// ^ We now have the parts
+
+		// Fix
+		key = decodeURIComponent(key);
+		value = decodeURIComponent(value);
+		try {
+		    // value can be converted
+		    value = eval(value);
+		} catch ( e ) {
+		    // value is a normal string
+		}
+
+		// Set
+		// window.console.log({'key':key,'value':value}, split);
+		var keys = key.split('.');
+		if ( keys.length === 1 )
+		{	// Simple
+			json[key] = value;
+		}
+		else
+		{	// Advanced (Recreating an object)
+			var path = '',
+				cmd = '';
+			// Ensure Path Exists
+			$.each(keys,function(ii,key){
+				path += '["'+key.replace(/"/g,'\\"')+'"]';
+				jsonCLOSUREGLOBAL = json; // we have made this a global as closure compiler struggles with evals
+				cmd = 'if ( typeof jsonCLOSUREGLOBAL'+path+' === "undefined" ) jsonCLOSUREGLOBAL'+path+' = {}';
+				eval(cmd);
+				json = jsonCLOSUREGLOBAL;
+				delete jsonCLOSUREGLOBAL;
+			});
+			// Apply Value
+			jsonCLOSUREGLOBAL = json; // we have made this a global as closure compiler struggles with evals
+			valueCLOSUREGLOBAL = value; // we have made this a global as closure compiler struggles with evals
+			cmd = 'jsonCLOSUREGLOBAL'+path+' = valueCLOSUREGLOBAL';
+			eval(cmd);
+			json = jsonCLOSUREGLOBAL;
+			delete jsonCLOSUREGLOBAL;
+			delete valueCLOSUREGLOBAL;
+		}
+		// ^ We now have the parts added to your JSON object
+	}
+	return json;
+};
 
 // perform a search for 'searchText' and optionally apply filters where
 // filter is an object like {topic: "NoSQL", language: "HTTP"}
@@ -53,6 +131,10 @@ var doSearch = function(searchText,filter, callback) {
   };
   
   $.ajax(obj).done(function(data) {
+    var qs = generateQueryString(searchText,filter);
+    if(qs.length>0) {
+      window.location.href= window.location.pathname+"#?"+qs;
+    }
     if (callback) {
       callback(null, data);
     }
@@ -193,16 +275,55 @@ var clearAllFilters = function() {
 }
 
 
+var generateQueryString = function(searchText, filter) {
+  var qs = ""
+  
+  if(searchText && searchText.length>0) {
+    qs += "searchText="+encodeURIComponent(searchText);
+  }
+  if(filter && filter.length>0) {
+    qs += "&filter="+JSON.stringify(filter);
+  }
+  return qs;
+}
+
+
 // onload perform a search for "everything"
 var onload = function() {
   searchText = "";
   filter = [];
-  var facets = doSearch(searchText, filter, function(err, data) {
-    allfacets = data.counts; // record the inital list of facets away for safekeeping
+  
+  // do first search to get all the facets
+  doSearch(searchText, filter, function(err, data) {
+    // record the inital list of facets away for safekeeping
+    allfacets = data.counts; 
     renderSerps(data,filter);
+
+    // parse the query string
+    var hash = location.hash;
+    if(hash) {
+      
+      // strip bits
+      hash = hash.replace(/^#/,"");
+      hash = hash.replace(/^\?/,"");
+      hash = hash.queryStringToJSON();
+      
+      // extract search and filter
+      searchText = (typeof hash.searchText=="string")?hash.searchText:"";
+      filter = (typeof hash.filter == "object")?hash.filter:[];
+      
+      // feed the search box
+      $('#searchtext').val(searchText);
+      
+      // do a second search
+      doSearch(searchText, filter, function(err, data) {
+        renderSerps(data, filter);
+      });
+    } 
   });
 }
 
+// when a checkbox is ticked
 var checktick = function(ctrl) {
   var facet = ctrl.getAttribute('data-facet');
   var value = ctrl.getAttribute('data-value');
@@ -215,4 +336,17 @@ var checktick = function(ctrl) {
   
 }
 
+// when the index page's search form is submitted
+var indexSearchSubmit = function() {
+  console.log("X");
+  return false;
+  var searchText = $('#bigsearchinput').val();
+  var qs = generateQueryString(searchText,[]);
+  console.log("resources.html#?"+qs);
+  window.location.href="resources.html#?"+qs;
+  return false;
+}
+
+
+// on load
 $(document).ready(onload);
